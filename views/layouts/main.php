ca7c0fe428263cdaa61f4f78903c79a9101b73fc
UTEHN <UTEHN@192.168.3.74>
2015-01-14 11:02:50 +0700

    2015-01-14

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -26,7 +26,7 @@ AppAsset::register($this);
     <div class="wrap">
         <?php
             NavBar::begin([
-                'brandLabel' => 'My Company',
+                'brandLabel' => 'Yii2 อ.หนองหาน',
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
